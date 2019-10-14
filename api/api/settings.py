775e1eea0082a9ac8beed5a8831068a7943e4fce
rogerio <rogerio@pa.sebrae.com.br>
2019-10-14 17:45:17 -0300

    Conexão como banco de dados Postgres

--- api/api/settings.py
+++ api/api/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'mapes'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASS', 'pg_pass'),
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
