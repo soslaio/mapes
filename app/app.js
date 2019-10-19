ebf97b854f7e4f9c09d0bfc3a6cf2ee41c5fa3c8
rogerio <rogerio@pa.sebrae.com.br>
2019-10-19 12:16:09 -0300

    Isolamento de responsabilidades entre métodos e arquivos

--- app/app.js
+++ app/app.js
@@ -1,27 +1,18 @@
 
+// Entry point da aplicação.
 $(document).ready(function () {
-    $('#medicos').select2();
+
+    // Pendura no botão o método que filtra as consultas.
     $('#pesquisar').click(filtrarConsultas)
 
+    // Carrega a lista de médicos.
     listarMedicos();
 });
 
-function consultar(query) {
-    const url = `http://localhost:8001/graphql/`;
-    var config = {
-        method: 'POST',
-        headers: new Headers({
-            "Content-Type": "application/json"
-        }),
-        body: JSON.stringify({
-            query: query
-        })
-    };
-    return fetch(url, config)
-        .then(response => response.json())
-}
 
 function listarMedicos() {
+
+    // Query padrão GraphQL que lista as informações dos médicos cadastrados.
     const query = `{
         todosMedicos {
             id
@@ -29,44 +20,51 @@ function listarMedicos() {
         }
     }`;
 
-    return consultar(query)
-        .then(jsonResponse => {
-            jsonResponse.data.todosMedicos.forEach(consulta => {
-                var objConsulta = new Option(consulta.nomeMedico, consulta.id, false, false);
-                $('#medicos').append(objConsulta).trigger('change');
-            })
-        })
+    // Executa a consulta de médicos na rota do GraphQL no servidor de API.
+    consultar(query)
+
+        // Passa a resposta da API para o método especialista em renderizar selects, informando
+        // quais das informações retornadas correspondem ao valor e ao texto do select.
+        .then(jsonResponse => renderizarSelect('#medicos', jsonResponse.data.todosMedicos, {
+            valor: 'id',
+            texto: 'nomeMedico'
+        }));
 }
 
+
 function filtrarConsultas() {
+
+    // Armazena a informação do médico selecionado.
     const codmedico = $('#medicos').val();
+
+    // Consulta padrão GraphQL filtrando pelo código do médico selecionado.
     const query = `{
         consultas(idMedico: "${codmedico}") {
             medico {
                 nomeMedico
             }
             numGuiaConsulta
-            dataConsulta
-            valorConsulta
-            gastoConsulta
+            dataConsultaFormatada
+            valorConsultaFormatado
+            gastoConsultaFormatado
             qtdeExames
         }
     }`;
 
+    // Executa a consulta de consultas na rota do GraphQL no servidor de API.
     consultar(query)
         .then(jsonResponse => {
-            return jsonResponse.data.consultas.reduce((html, consulta) => html += `
-                    <tr>
-                        <td>${consulta.medico.nomeMedico}</td>
-                        <td>${consulta.numGuiaConsulta}</td>
-                        <td>${consulta.dataConsulta}</td>
-                        <td>${consulta.valorConsulta}</td>
-                        <td>${consulta.gastoConsulta}</td>
-                        <td>${consulta.qtdeExames}</td>
-                    </tr>
-                `, '')
-        })
-        .then(html => {
-            document.querySelector('#consultas').innerHTML = html;
+
+            // Extrai o nome do médico do objeto com as informações do mesmo
+            // e substitui esse objeto pelo nome extraído.
+            const dadosAjustados = jsonResponse.data.consultas.map(dado => {
+                return {
+                    ...dado,
+                    medico: dado.medico.nomeMedico
+                }
+            });
+
+            // Passa os dados ajustados pro método especialista em renderizar tabelas.
+            renderizarTabela('#consultas', dadosAjustados);
         })
 }
---
+++ app/util.js
@@ -0,0 +1,93 @@
+
+/**
+ * Função especialista em efetuar querys remotas no servidor de API GraphQL.
+ * 
+ * @param {string} query Query padrão GraphQL para ser executada no servidor de API.
+ */
+async function consultar(query) {
+
+    // URL base para todas as consultas ao GraphQL.
+    const url = `http://localhost:8001/graphql/`;
+
+    // Configurações da requisição, incluindo método de consulta,
+    // cabeçalho HTML e dados JSON da consulta.
+    var requestConfig = {
+        method: 'POST',
+        headers: new Headers({
+            "Content-Type": "application/json"
+        }),
+        body: JSON.stringify({
+            query: query
+        })
+    };
+
+    // Retorna uma promisse que passa por um processo de análise da resposta
+    // e de exibição de erro, caso algo errado aconteça.
+    return fetch(url, requestConfig)
+        .then(analisarRespostaHTTP)
+        .then(response => response.json())
+        .catch(exibirErro);
+}
+
+
+/**
+ * Função especialista em renderizar dados num select HTML.
+ * 
+ * @param {string} seletor Seletor CSS do elemento select onde os dados devem ser renderizados.
+ * @param {Array} dados    Array de objetos com as informações a serem renderizadas.
+ * @param {object} mapa    Objeto que relaciona a propriedade do objeto e o valor e texto exibido pelo select.
+ */
+function renderizarSelect(seletor, dados, mapa) {
+
+    dados.forEach(dado => {
+
+        // Cria o elemento option e adiciona ao select.
+        var objConsulta = new Option(dado[mapa.texto], dado[mapa.valor], false, false);
+        $(seletor).append(objConsulta);
+    });
+}
+
+
+/**
+ * Função especialista em renderizar dados no corpo de tabelas HTML.
+ * 
+ * @param {string} seletor Seletor CSS do elemento tbody onde os dados devem ser renderizados.
+ * @param {Array} dados    Dados plano a serem renderizados na tabela.
+ */
+function renderizarTabela(seletor, dados) {
+
+    const htmlBody = dados.reduce((htmlLinha, dado) => {
+        return htmlLinha += `<tr>
+            ${Object.keys(dado).reduce((htmlColuna, coluna) => htmlColuna += `<td>${dado[coluna]}</td>`, '')}
+        </tr>`;
+    }, '');
+    $(seletor).html(htmlBody);
+}
+
+
+/**
+ * Função que exibe em tela uma exceção lançada.
+ * 
+ * @param {Error} erro Exceção lançada pelo sistema.
+ */
+function exibirErro(erro) {
+
+    alert(`Falha ao consultar: ${erro.message}.`);
+}
+
+
+/**
+ * Função que analisa uma resposta HTTP a procura de erros.
+ * 
+ * @param {object} resposta Object de resposta HTTP da fetch API.
+ */
+async function analisarRespostaHTTP(resposta) {
+
+    // Se a resposta estiver OK, retorna a resposta.
+    if (resposta.ok) {
+        return resposta;
+    }
+
+    // Lança uma exceção caso a resposta não esteja OK.
+    throw new Error(resposta.statusText);
+}
