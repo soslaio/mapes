555b7cfcbd587ce10e3aaae9bcba60a2dc1d4437
rogerio <rogerio@pa.sebrae.com.br>
2019-10-20 18:39:16 -0300

    Criação do arquivo de teste e script para rodá-lo

--- api/core/tests.py
+++ api/core/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from .models import Consulta, Medico
+
+
+class CoreModelTests(TestCase):
+    def setUp(self):
+        medico = Medico.objects.create(nome_medico="Breno Murphy")
+        Consulta.objects.create(medico=medico, data_consulta="2019-01-01", valor_consulta=90)
+
+    def test_valor_da_consulta_e_90(self):
+        consulta_breno = Consulta.objects.get(medico__nome_medico="Breno Murphy")
+        print(consulta_breno)
+        self.assertTrue(True)
